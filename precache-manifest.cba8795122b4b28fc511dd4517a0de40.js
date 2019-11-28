417d1c6466866355b38cf5081ba19b3586864c85
AidiRi <rivera.aidi@gmail.com>
2019-11-27 18:11:36 -0800

    Updates

---
+++ precache-manifest.cba8795122b4b28fc511dd4517a0de40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b4ed53ae2b17b1457677ec15beb32b2",
+    "url": "/hb-login-challenge/index.html"
+  },
+  {
+    "revision": "eeb71ca768beb3f8775a",
+    "url": "/hb-login-challenge/static/css/main.1b1e9882.chunk.css"
+  },
+  {
+    "revision": "20fef5d7743aebd06a8a",
+    "url": "/hb-login-challenge/static/js/2.4230e0a3.chunk.js"
+  },
+  {
+    "revision": "eeb71ca768beb3f8775a",
+    "url": "/hb-login-challenge/static/js/main.0c3d2e33.chunk.js"
+  },
+  {
+    "revision": "fd9a8f44f1b05f67a781",
+    "url": "/hb-login-challenge/static/js/runtime-main.3a418675.js"
+  }
+]);
